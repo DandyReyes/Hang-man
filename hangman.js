2032dd9b0431d9b3cd14299332728e6f11d3fdca
Dandy <dandyjreyes@gmail.com>
2019-06-26 12:51:36 -0700

    Put code into functions

--- hangman.js
+++ hangman.js
@@ -8,36 +8,63 @@ const wordBank = [
   'react',
   'computer',
 ];
-function game(wordLength, revealedWord) {
-  const guessedWord = [];
-  let failures = 0;
-  let letterCount = 0;
 
+function startingLines(guessedWord, wordLength) {
   for (let i = 0; i < wordLength; i++) {
     guessedWord.push('_');
   }
+}
 
-  console.log(guessedWord.join(' '));
+function winOrLost(
+  failures,
+  letterCount,
+  wordLength,
+  revealedWord,
+  guessedWord
+) {
   while (guessedWord.join() !== revealedWord.join() && failures < 5) {
     const userLetter = readlineSync.question('Guess a letter: ');
+
     for (let i = 0; i < wordLength; i++) {
       if (userLetter === revealedWord[i]) {
         guessedWord[i] = userLetter;
         ++letterCount;
       }
     }
+
     if (letterCount === 0) {
       ++failures;
       console.log(`You have ${failures} strike(s)`);
     }
+
     letterCount = 0;
     console.log(guessedWord.join(' '));
   }
+
   if (guessedWord.join() === revealedWord.join()) {
     console.log('You Won!!! :)');
   } else {
     console.log('You Lost :(');
   }
+}
+
+function game(wordLength, revealedWord) {
+  const guessedWord = [];
+  const failures = 0;
+  const letterCount = 0;
+
+  startingLines(guessedWord, wordLength);
+
+  console.log(guessedWord.join(' '));
+
+  winOrLost(
+    failures,
+    letterCount,
+    wordLength,
+    revealedWord,
+    guessedWord,
+    letterCount
+  );
 }
 
 function hangman() {
